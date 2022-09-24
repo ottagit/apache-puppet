b7efbabfd44386e82b66afb6e2e1f7d57d0a0054
ottagit <ottaosawa@gmail.com>
2022-09-24 10:10:07 +0000

    add a service class

---
+++ manifests/service.pp
@@ -0,0 +1,11 @@
+# @summary Allows for the Apache service to restart when tiggered
+#
+class apache::service {
+  service {
+    "$apache::service_name":
+      alias      => 'apache_service',
+      ensure     => $apache::service_ensure,
+      enable     => $apache::service_enable,
+      hasrestart => true,
+  }
+}
