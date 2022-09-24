4ef9b1c0adb4ffd8a37c80807a9b1987f7d81465
ottagit <ottaosawa@gmail.com>
2022-09-24 08:45:25 +0000

    refactor params.pp in favor of Hiera

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,12 +1,9 @@
 # @summary
 #   Installs the base Apache module
 
-class apache::install (
-  $install_name = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure,
-) inherits apache::params {
+class apache::install {
   package {
-    "${install_name}":
-      ensure   => $install_ensure,
+    "${apache::install_name}":
+      ensure   => $apache::install_ensure,
   }
 }
